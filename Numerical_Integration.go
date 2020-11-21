3ec839a7d3d6565bd2f1a2a22088d39d4549bf17
Nikita <nike04@mail.ru>
2020-11-21 18:19:02 +0500

    изменил вывод

--- Numerical_Integration.go
+++ Numerical_Integration.go
@@ -5,24 +5,24 @@ import (
 	"math"
 )
 
-var variant int = 3
-var eps float64 = math.Pow(10, -8)
+var variant int = 18
+var eps float64 = 10e-8 //math.Pow(10, -8)
 
 //исходная функция
 func f(x float64) float64 {
 	if variant == 3 {
 		return math.Pow(5, x) - 3
 	} else {
-		return math.Pow(3, x) - 2 * x + 5
+		return math.Pow(3, x) - 2*x + 5
 	}
 }
 
 //производная исходной функции
 func dF(x float64) float64 {
-	if variant ==3 {
-		return math.Log(5)*math.Pow(5,x)
+	if variant == 3 {
+		return math.Log(5) * math.Pow(5, x)
 	} else {
-		return math.Log(3) * math.Pow(3,x)-2
+		return math.Log(3)*math.Pow(3, x) - 2
 	}
 }
 
@@ -54,26 +54,26 @@ func partIntegral(N int, a, b, h float64, method int, k *int) float64 {
 	//формула Симпсона
 	case 3:
 		{
-			for i:=0; i<N; i++ {
-				var _a float64 = a+float64(i)*h
-				var _b float64 = a+float64(i+1)*h
-				sum += ((_b-_a)/6.)*(f(_a)+4*f((_a+_b)/2.)+f(_b))
+			for i := 0; i < N; i++ {
+				var _a float64 = a + float64(i)*h
+				var _b float64 = a + float64(i+1)*h
+				sum += ((_b - _a) / 6.) * (f(_a) + 4*f((_a+_b)/2.) + f(_b))
 			}
-			*k+=N
+			*k += N
 			break
 		}
 	//формула Гаусса
 	case 4:
 		{
 			var iterX, a02, a1 float64
-			iterX=0
-			a02 = 5./9
-			a1 = 8./9
-			for i:=0; i<N; i++ {
-				iterX = a + (2*float64(i) + 1)*h/2
-				sum += h/2.*(a02*f(iterX-h*math.Sqrt(0.6)/2.)+
-						a1*f(iterX)+a02*f(iterX+h*math.Sqrt(0.6)/2.))
-				*k+=3
+			iterX = 0
+			a02 = 5. / 9
+			a1 = 8. / 9
+			for i := 0; i < N; i++ {
+				iterX = a + (2*float64(i)+1)*h/2
+				sum += h / 2. * (a02*f(iterX-h*math.Sqrt(0.6)/2.) +
+					a1*f(iterX) + a02*f(iterX+h*math.Sqrt(0.6)/2.))
+				*k += 3
 			}
 			break
 		}
@@ -102,28 +102,28 @@ func calculateIntegral(a, b float64, method int) {
 	case 1:
 		{
 			fmt.Println("Trapezium method:")
-			tet = 1./3
+			tet = 1. / 3
 			count = 0
 			break
 		}
 	case 2:
 		{
 			fmt.Println("Modified trapezium method:")
-			tet = 1./3
+			tet = 1. / 3
 			count = 0
 			break
 		}
 	case 3:
 		{
 			fmt.Println("Simpson Method:")
-			tet = 1./15
+			tet = 1. / 15
 			count = 0
 			break
 		}
 	case 4:
 		{
 			fmt.Println("Gauss method:")
-			tet = 1./63
+			tet = 1. / 63
 			count = 0
 			break
 		}
@@ -132,21 +132,21 @@ func calculateIntegral(a, b float64, method int) {
 
 	//s0 = partIntegral(N, a, b, h, method, count)
 	//s1 = partIntegral(N*2, a, b, h/2, method, count)
-	k=0
+	k = 0
 
-	for math.Abs(err)>eps {
+	for math.Abs(err) > eps {
 		term = sum
-		h = (b - a)/float64(N)
+		h = (b - a) / float64(N)
 		sum = partIntegral(N, a, b, h, method, &count)
 		err = (sum - term) * tet
 		//эмпирическая оценка порядка аппроксимации
-		k = math.Log((sum-s0)/(s1-s0)-1.)/math.Log(0.5)
-		s0=s1
-		s1=sum
+		k = math.Log((sum-s0)/(s1-s0)-1.) / math.Log(0.5)
+		s0 = s1
+		s1 = sum
 		print(N, h, sum, err, k)
-		N*=2
+		N *= 2
 	}
-	if method!=4 {
+	if method != 4 {
 		count += 2
 	}
 	fmt.Println("\nResult: ", sum, "\nNumber of requests: ", count, "\n\n")
@@ -154,8 +154,8 @@ func calculateIntegral(a, b float64, method int) {
 
 func main() {
 	var a, b float64
-	a=1
-	b=2
+	a = 1
+	b = 2
 	calculateIntegral(a, b, 1)
 	calculateIntegral(a, b, 2)
 	calculateIntegral(a, b, 3)
